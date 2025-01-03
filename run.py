b9c1830abf129263284db35e7939c2b28671d280
Hào Lâm <haolamnm@gmail.com>
2025-01-04 03:45:13 +0700

    simplify run command

--- run.py
+++ run.py
@@ -1,23 +1,9 @@
-import sys
 from app import create_app
-from config import Config, DevelopmentConfig
+from config import Config
 
 
-def main():
-	if len(sys.argv) > 2:
-		print('Usage: python run.py [mode]')
-		return
-
-	mode = sys.argv[1] if len(sys.argv) == 2 else 'production'
-	if mode == 'dev':
-		app = create_app(DevelopmentConfig)
-		app.run(debug=True)
-	elif mode == 'production':
-		app = create_app(Config)
-		app.run()
-	else:
-		print('Available modes: dev, production')
+app = create_app(Config)
 
 
 if __name__ == '__main__':
-	main()
+	app.run()
