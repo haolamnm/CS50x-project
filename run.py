ade5cd3127787235b7b98f84b5a9002578feb6cd
Hào Lâm <haolamnm@gmail.com>
2025-01-04 01:09:26 +0700

    refactor run command

--- run.py
+++ run.py
@@ -4,19 +4,19 @@ from config import Config, DevelopmentConfig
 
 
 def main():
-	if len(sys.argv) != 2:
+	if len(sys.argv) > 2:
 		print('Usage: python run.py [mode]')
 		return
 
-	mode = sys.argv[1]
-	if mode == 'development':
+	mode = sys.argv[1] if len(sys.argv) == 2 else 'production'
+	if mode == 'dev':
 		app = create_app(DevelopmentConfig)
 		app.run(debug=True)
 	elif mode == 'production':
 		app = create_app(Config)
 		app.run()
 	else:
-		print('Available modes: development, production')
+		print('Available modes: dev, production')
 
 
 if __name__ == '__main__':
