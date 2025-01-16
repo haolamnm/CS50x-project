fa08244ae14b714ee0609084b86f17d460383172
haolamnm <haolamnm@gmail.com>
2025-01-16 22:20:13 +0700

    Refactor update route tests

--- tests/test_route_update.py
+++ tests/test_route_update.py
@@ -2,7 +2,6 @@ from unittest import main
 from pytest import mark
 from tests.cases import (
     INVALID_LOGIN_PASSWORD_TEST_CASES,
-    INVALID_USERNAME_TEST_CASES,
     INVALID_EMAIL_TEST_CASES,
     INVALID_PASSWORD_TEST_CASES
 )
@@ -17,112 +16,6 @@ class TestRouteUpdate(TestRouteBase):
     This class contains the test cases for the update route.
     """
 
-    def test_update_username_with_valid_username(self) -> None:
-        """
-        Test the username update with valid username
-
-        The test will try to update the username with a valid username then check the response.
-
-        :return: None
-        """
-        self.login(
-            login_type='username_login',
-            identifier=self.test_username,
-            password=self.test_password
-        )
-        response = self.update(
-            update_type='username_update',
-            password=self.test_password,
-            new_value=self.test_new_username
-        )
-        self.check_status_code(response, 200)
-        self.check_title(response, 'Profile')
-        self.check_flash(response, 'Profile updated successfully', 'success')
-
-        with self.client.session_transaction() as session:
-            id = session['user_id']
-
-        user = self.check_user(
-            id=id,
-            username=self.test_new_username,
-            email=self.test_email,
-            password=self.test_password
-        )
-        self.check_session_exists(user)
-
-
-    def test_update_username_with_invalid_password(self) -> None:
-        """
-        Test the username update with invalid password
-
-        Each test case will try to update the username with an invalid password then check the response.
-
-        :return: None
-        """
-        self.login(
-            login_type='username_login',
-            identifier=self.test_username,
-            password=self.test_password
-        )
-        for password, expected in INVALID_LOGIN_PASSWORD_TEST_CASES:
-            with self.subTest(password=password, expected=expected):
-                response = self.update(
-                    update_type='username_update',
-                    password=password,
-                    new_value=self.test_new_username
-                )
-                self.check_status_code(response, 200)
-                self.check_title(response, 'Profile')
-                self.check_flash(response, expected, 'warning')
-
-                with self.client.session_transaction() as session:
-                    id = session['user_id']
-
-                user = self.check_user(
-                    id=id,
-                    username=self.test_username,
-                    email=self.test_email,
-                    password=self.test_password
-                )
-                self.check_session_exists(user)
-
-
-    def test_update_username_with_invalid_username(self) -> None:
-        """
-        Test the username update with invalid username
-
-        Each test case will try to update the username with an invalid username then check the response.
-
-        :return: None
-        """
-        self.login(
-            login_type='username_login',
-            identifier=self.test_username,
-            password=self.test_password
-        )
-        for new_username, expected in INVALID_USERNAME_TEST_CASES:
-            with self.subTest(new_username=new_username, expected=expected):
-                response = self.update(
-                    update_type='username_update',
-                    password=self.test_password,
-                    new_value=new_username
-                )
-                self.check_status_code(response, 200)
-                self.check_title(response, 'Profile')
-                self.check_flash(response, expected, 'warning')
-
-                with self.client.session_transaction() as session:
-                    id = session['user_id']
-
-                user = self.check_user(
-                    id=id,
-                    username=self.test_username,
-                    email=self.test_email,
-                    password=self.test_password
-                )
-                self.check_session_exists(user)
-
-
     def test_update_email_with_valid_email(self) -> None:
         """
         Test the email update with valid email
@@ -132,25 +25,18 @@ class TestRouteUpdate(TestRouteBase):
         :return: None
         """
         self.login(
-            login_type='username_login',
-            identifier=self.test_username,
+            email=self.test_email,
             password=self.test_password
         )
-        response = self.update(
-            update_type='email_update',
-            password=self.test_password,
-            new_value=self.test_new_email
+        response = self.update_email(
+            new_email=self.test_new_email,
+            password=self.test_password
         )
         self.check_status_code(response, 200)
-        self.check_title(response, 'Profile')
+        self.check_title(response, 'Profile | Pomodoro 50')
         self.check_flash(response, 'Profile updated successfully', 'success')
 
-        with self.client.session_transaction() as session:
-            id = session['user_id']
-
         user = self.check_user(
-            id=id,
-            username=self.test_username,
             email=self.test_new_email,
             password=self.test_password
         )
@@ -166,27 +52,20 @@ class TestRouteUpdate(TestRouteBase):
         :return: None
         """
         self.login(
-            login_type='username_login',
-            identifier=self.test_username,
+            email=self.test_email,
             password=self.test_password
         )
         for password, expected in INVALID_LOGIN_PASSWORD_TEST_CASES:
             with self.subTest(password=password, expected=expected):
-                response = self.update(
-                    update_type='email_update',
-                    password=password,
-                    new_value=self.test_new_email
+                response = self.update_email(
+                    new_email=self.test_new_email,
+                    password=password
                 )
                 self.check_status_code(response, 200)
-                self.check_title(response, 'Profile')
+                self.check_title(response, 'Profile | Pomodoro 50')
                 self.check_flash(response, expected, 'warning')
 
-                with self.client.session_transaction() as session:
-                    id = session['user_id']
-
                 user = self.check_user(
-                    id=id,
-                    username=self.test_username,
                     email=self.test_email,
                     password=self.test_password
                 )
@@ -202,34 +81,27 @@ class TestRouteUpdate(TestRouteBase):
         :return: None
         """
         self.login(
-            login_type='username_login',
-            identifier=self.test_username,
+            email=self.test_email,
             password=self.test_password
         )
         for new_email, expected in INVALID_EMAIL_TEST_CASES:
             with self.subTest(new_email=new_email, expected=expected):
-                response = self.update(
-                    update_type='email_update',
-                    password=self.test_password,
-                    new_value=new_email
+                response = self.update_email(
+                    new_email=new_email,
+                    password=self.test_password
                 )
                 self.check_status_code(response, 200)
-                self.check_title(response, 'Profile')
+                self.check_title(response, 'Profile | Pomodoro 50')
                 self.check_flash(response, expected, 'warning')
 
-                with self.client.session_transaction() as session:
-                    id = session['user_id']
-
                 user = self.check_user(
-                    id=id,
-                    username=self.test_username,
                     email=self.test_email,
                     password=self.test_password
                 )
                 self.check_session_exists(user)
 
 
-    def test_update_password_with_valid_password(self) -> None:
+    def test_update_password_with_valid_new_password(self) -> None:
         """
         Test the password update with valid password
 
@@ -238,26 +110,19 @@ class TestRouteUpdate(TestRouteBase):
         :return: None
         """
         self.login(
-            login_type='username_login',
-            identifier=self.test_username,
+            email=self.test_email,
             password=self.test_password
         )
-        response = self.update(
-            update_type='password_update',
-            password=self.test_password,
-            new_value=self.test_new_password,
-            new_confirm_value=self.test_new_password
+        response = self.update_password(
+            new_password=self.test_new_password,
+            new_confirmation=self.test_new_password,
+            password=self.test_password
         )
         self.check_status_code(response, 200)
-        self.check_title(response, 'Profile')
+        self.check_title(response, 'Profile | Pomodoro 50')
         self.check_flash(response, 'Profile updated successfully', 'success')
 
-        with self.client.session_transaction() as session:
-            id = session['user_id']
-
         user = self.check_user(
-            id=id,
-            username=self.test_username,
             email=self.test_email,
             password=self.test_new_password
         )
@@ -273,28 +138,21 @@ class TestRouteUpdate(TestRouteBase):
         :return: None
         """
         self.login(
-            login_type='username_login',
-            identifier=self.test_username,
+            email=self.test_email,
             password=self.test_password
         )
         for password, expected in INVALID_LOGIN_PASSWORD_TEST_CASES:
             with self.subTest(password=password, expected=expected):
-                response = self.update(
-                    update_type='password_update',
-                    password=password,
-                    new_value=self.test_new_password,
-                    new_confirm_value=self.test_new_password
+                response = self.update_password(
+                    new_password=self.test_new_password,
+                    new_confirmation=self.test_new_password,
+                    password=password
                 )
                 self.check_status_code(response, 200)
-                self.check_title(response, 'Profile')
+                self.check_title(response, 'Profile | Pomodoro 50')
                 self.check_flash(response, expected, 'warning')
 
-                with self.client.session_transaction() as session:
-                    id = session['user_id']
-
                 user = self.check_user(
-                    id=id,
-                    username=self.test_username,
                     email=self.test_email,
                     password=self.test_password
                 )
@@ -310,66 +168,25 @@ class TestRouteUpdate(TestRouteBase):
         :return: None
         """
         self.login(
-            login_type='username_login',
-            identifier=self.test_username,
+            email=self.test_email,
             password=self.test_password
         )
         for new_password, new_confirmation, expected in INVALID_PASSWORD_TEST_CASES:
             with self.subTest(new_password=new_password, new_confirmation=new_confirmation, expected=expected):
-                response = self.update(
-                    update_type='password_update',
-                    password=self.test_password,
-                    new_value=new_password,
-                    new_confirm_value=new_confirmation
+                response = self.update_password(
+                    new_password=new_password,
+                    new_confirmation=new_confirmation,
+                    password=self.test_password
                 )
                 self.check_status_code(response, 200)
-                self.check_title(response, 'Profile')
+                self.check_title(response, 'Profile | Pomodoro 50')
                 self.check_flash(response, expected, 'warning')
 
-                with self.client.session_transaction() as session:
-                    id = session['user_id']
-
                 user = self.check_user(
-                    id=id,
-                    username=self.test_username,
                     email=self.test_email,
                     password=self.test_password
                 )
                 self.check_session_exists(user)
-
-
-    def test_update_with_invalid_update_type(self) -> None:
-        """
-        Test the update route with invalid update type (not username, email, or password)
-
-        The test will try to update the profile with an invalid update type then check the response.
-
-        :return: None
-        """
-        self.login(
-            login_type='username_login',
-            identifier=self.test_username,
-            password=self.test_password
-        )
-        response = self.update(
-            update_type='invalid_update',
-            password=self.test_password,
-            new_value=self.test_new_username
-        )
-        self.check_status_code(response, 200)
-        self.check_title(response, 'Profile')
-        self.check_flash(response, 'Invalid update type', 'warning')
-
-        with self.client.session_transaction() as session:
-            id = session['user_id']
-
-        user = self.check_user(
-            id=id,
-            username=self.test_username,
-            email=self.test_email,
-            password=self.test_password
-        )
-        self.check_session_exists(user)
 
 
 if __name__ == '__main__':
