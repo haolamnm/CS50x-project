21db968826d1a9f1cac1aca347ff80f5d1e38b96
haolamnm <haolamnm@gmail.com>
2025-01-16 22:19:39 +0700

    Refactor login route tests

--- tests/test_route_login.py
+++ tests/test_route_login.py
@@ -1,7 +1,6 @@
 from unittest import main
 from pytest import mark
 from tests.cases import (
-    INVALID_LOGIN_USERNAME_TEST_CASES,
     INVALID_LOGIN_EMAIL_TEST_CASES,
     INVALID_LOGIN_PASSWORD_TEST_CASES
 )
@@ -16,25 +15,6 @@ class TestRouteLogin(TestRouteBase):
     This class contains the test cases for the login route.
     """
 
-    def test_login_with_valid_username(self) -> None:
-        """
-        Test the login with valid username
-
-        The test will try to login with a valid username then check the response.
-
-        :return: None
-        """
-        response = self.login(
-            login_type='username_login',
-            identifier=self.test_username,
-            password=self.test_password
-        )
-        self.check_status_code(response, 200)
-        self.check_title(response, 'Home')
-        self.check_flash(response, 'Logged in successfully', 'success')
-        self.check_session_exists(self.test_user)
-
-
     def test_login_with_valid_email(self) -> None:
         """
         Test the login with valid email
@@ -44,35 +24,13 @@ class TestRouteLogin(TestRouteBase):
         :return: None
         """
         response = self.login(
-            login_type='email_login',
-            identifier=self.test_email,
+            email=self.test_email,
             password=self.test_password
         )
         self.check_status_code(response, 200)
-        self.check_title(response, 'Home')
+        self.check_title(response, 'Home | Pomodoro 50')
         self.check_flash(response, 'Logged in successfully', 'success')
         self.check_session_exists(self.test_user)
-
-
-    def test_login_with_invalid_username(self) -> None:
-        """
-        Test the login route with invalid username
-
-        Each test case will try to login with an invalid username then check the response.
-
-        :return: None
-        """
-        for username, expected in INVALID_LOGIN_USERNAME_TEST_CASES:
-            with self.subTest(username=username, expected=expected):
-                response = self.login(
-                    login_type='username_login',
-                    identifier=username,
-                    password=self.test_password
-                )
-                self.check_status_code(response, 200)
-                self.check_title(response, 'Log In')
-                self.check_flash(response, expected, 'warning')
-                self.check_session_absent()
 
 
     def test_login_with_invalid_email(self) -> None:
@@ -86,12 +44,11 @@ class TestRouteLogin(TestRouteBase):
         for email, expected in INVALID_LOGIN_EMAIL_TEST_CASES:
             with self.subTest(email=email, expected=expected):
                 response = self.login(
-                    login_type='email_login',
-                    identifier=email,
+                    email=email,
                     password=self.test_password
                 )
                 self.check_status_code(response, 200)
-                self.check_title(response, 'Log In')
+                self.check_title(response, 'Log In | Pomodoro 50')
                 self.check_flash(response, expected, 'warning')
                 self.check_session_absent()
 
@@ -107,33 +64,13 @@ class TestRouteLogin(TestRouteBase):
         for password, expected in INVALID_LOGIN_PASSWORD_TEST_CASES:
             with self.subTest(password=password, expected=expected):
                 response = self.login(
-                    login_type='username_login',
-                    identifier=self.test_username,
+                    email=self.test_email,
                     password=password
                 )
                 self.check_status_code(response, 200)
-                self.check_title(response, 'Log In')
+                self.check_title(response, 'Log In | Pomodoro 50')
                 self.check_flash(response, expected, 'warning')
                 self.check_session_absent()
-
-
-    def test_login_with_invalid_login_type(self) -> None:
-        """
-        Test the login route with invalid login type
-
-        The test will try to login with an invalid login type then check the response.
-
-        :return: None
-        """
-        response = self.login(
-            login_type='invalid_login',
-            identifier=self.test_username,
-            password=self.test_password
-        )
-        self.check_status_code(response, 200)
-        self.check_title(response, 'Log In')
-        self.check_flash(response, 'Invalid login type', 'warning')
-        self.check_session_absent()
 
 
 if __name__ == '__main__':
